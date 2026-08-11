e0e51c8f685ab55f9aee7e2cec7c06c3325b4344
Mahek Mulla <mahekmulla491@gmail.com>
2026-08-11 20:37:41 +0530

    Migrate to Google GenAI

--- src/model.py
+++ src/model.py
@@ -1,13 +1,17 @@
 import os
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
 
 def get_response(prompt):
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
     return response.text
